74e5ace68c26c047e76024b08fa9bf712758d906
lukaknezic <luka.knezic@gmail.com>
2021-02-27 15:08:58 +0100

    Update

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/katzen/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/katzen/Documents/RandomColor/example"
-export "FLUTTER_TARGET=/Users/katzen/Documents/RandomColor/example/lib/main.dart"
+export "FLUTTER_ROOT=/Users/snoopy/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/snoopy/Documents/github/RandomColor/example"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
